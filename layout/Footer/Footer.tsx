9286b4ea15775dfeb3538ef0cd71a25090294024
paul-frdev <pavelzadyrko@gmail.com>
2022-03-29 09:44:36 +0300

    add: styles html

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -7,8 +7,14 @@ import cn from 'classnames';
 
 export const Footer = ({ ...props }: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            Footer
+        <div {...props} >
+            <div className={styles.footer}>
+                <p className={styles.footerRights}>OwlTop © <span>{new Date().getFullYear()}</span> Все права защищены</p>
+                <div className={styles.footerPolicy}>
+                    <a href="#" className={styles.footerLink}>Пользовательское соглашение</a>
+                    <a href="#" className={styles.footerLink}>Политика конфиденциальности</a>
+                </div>
+            </div>
         </div>
     );
 };
