f5343f4f1fb1eefbcceddac6959fd8d507eff8d6
paul-frdev <pavelzadyrko@gmail.com>
2022-04-02 14:40:39 +0300

    add: textarea

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { GetStaticProps } from 'next';
 import { useState } from 'react';
-import { Button, Htag, Input, Ptag, Rating, Tag } from '../components';
+import { Button, Htag, Input, Ptag, Rating, Tag, TextArea } from '../components';
 import { WithLayout } from '../layout/Layout';
 import axios from 'axios';
 import { MenuItem } from '../interfaces/menu.interface';
@@ -11,6 +11,7 @@ const Home = ({ menu }: HomeProps): JSX.Element => {
     < >
       <Rating rating={rating} isEditable={true} setRating={setRating} />
       <Input placeholder='текст'/>
+      <TextArea placeholder='Текст отзыва'/>
     </>
   );
 };
